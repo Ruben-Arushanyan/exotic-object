5d5cae639d91eb6762c7f6ca90e54d64d790624f
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-12-26 16:14:36 +0400

    CallNotAllowedError class

--- src/errors.js
+++ src/errors.js
@@ -1,8 +1,19 @@
 
-class IncorrectOperationError extends Error {
-  constructor(message) {
-    super(message);
+class OperationNotAllowedError extends Error {
+  constructor(options) {
+    const operation = options?.operation ?? "Operation"
+    const message = options?.message ?? `${operation} is Not Allowed`
+    super(message)
   }
+}
+
+class CallNotAllowedError extends OperationNotAllowedError {
+    constructor(options) {
+        super({
+            message: options?.message,
+            operation: "Call",
+        });
+    }
 }
 
 module.exports = {
