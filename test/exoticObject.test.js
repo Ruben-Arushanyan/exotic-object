76ec89698da7e016a1a17de9ba25a8fd1b43dd30
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-12-28 15:56:22 +0400

    test improvements

--- test/exoticObject.test.js
+++ test/exoticObject.test.js
@@ -6,26 +6,5 @@ import {
 test('exoticObject', () => {
     {
         const e_o_1 = exoticObject()
-        const e_o_2 = exoticObject({
-            data: {name: "Ruben"},
-            operation: {
-                call: (target, argumentsList, thisArg) => {
-                   // ....
-                },
-            },
-            propertyOperation: {
-                title: {
-                  get: (target, property, receiver) => {
-                    // ...
-                  },
-                },
-                '*': {
-                   get: (target, property, receiver) => {
-                    // ...
-                  },
-                }
-              }
-        })
-
     }
 })
